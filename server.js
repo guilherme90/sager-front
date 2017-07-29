b64eafd486c7b65ee08a35c881f381ebc505ef75
Guilherme Nogueira <guilhermenogueira90@gmail.com>
2017-07-29 17:06:03 -0300

    Changes to prod

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const webpackConfig = require('./webpack.config')
 
 const compiler = webpack(webpackConfig)
 const app = express()
-const __PORT__ = process.env.PORT || 8001
+const __PORT__ = process.env.PORT
 
 if (process.env.NODE_ENV === 'development') {
   const opn = require('opn')
